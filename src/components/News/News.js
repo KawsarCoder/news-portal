1bef0183f53c5e0935f0d620425ad6bdbe0aed89
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2023-04-01 02:23:27 +0600

    create news portal app & add some components

---
+++ src/components/News/News.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import NewsItems from '../NewsItems/NewsItems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+            This is news component
+            <NewsItems/>
+            <NewsItems/>
+            <NewsItems/>
+            <NewsItems/>
+            <NewsItems/>
+            <NewsItems/>
+      </div>
+    )
+  }
+}
+
+export default News
