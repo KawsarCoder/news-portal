83b234891f00730954fbda6a113cdefe8159e36b
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2023-04-01 21:38:22 +0600

    loading spinner added and updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import NavBar from './components/NavBar/NavBar'
-import News from './components/News/News'
+import React, { Component } from "react";
+import NavBar from "./components/NavBar/NavBar";
+import News from "./components/News/News";
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <News/>
+        <NavBar pageSize={21} />
+        <News />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -10,11 +10,9 @@ export class News extends Component {
       page: 1,
     };
   }
-
   // fetch all news data
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=87e145ee21b44cde98aec3b98deebcc1&page=1&pageSize=15";
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=87e145ee21b44cde98aec3b98deebcc1&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
@@ -27,7 +25,7 @@ export class News extends Component {
   handlePrevClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=87e145ee21b44cde98aec3b98deebcc1&page=${
       this.state.page - 1
-    }&pageSize=15`;
+    }&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
 
@@ -38,11 +36,14 @@ export class News extends Component {
   };
 
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 15)) {
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
     } else {
       let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=87e145ee21b44cde98aec3b98deebcc1&page=${
         this.state.page + 1
-      }&pageSize=15`;
+      }&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let parsedData = await data.json();
 
@@ -54,10 +55,42 @@ export class News extends Component {
   };
 
   render() {
+    console.log(this.state.articles);
     return (
       <div className="container my-3">
-        <h2>NewsPortal - Top Headlines</h2>
-
+        <h1 className="text-center">NewsPortal - Top Headlines</h1>
+        {/* loading spinner start */}
+        {this.state.articles.length < 1 ? (
+          <div className="d-flex justify-content-center mt-5">
+            <div class="spinner-grow text-primary" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-secondary" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-success" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-danger" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-warning" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-info" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-dark" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="spinner-grow text-light" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+        {/* loading spinner end  */}
         <div className="row">
           {this.state.articles.map((el) => {
             return (
